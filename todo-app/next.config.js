805d6650ab23726da1d581e4feb347ba569634e4
nieje <jenni.niemela@digitalistgroup.com>
2023-11-08 12:25:09 +0200

    remoed unused css and added styled componets compiler to next.config

--- todo-app/next.config.js
+++ todo-app/next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    compiler: {
+      styledComponents: true,
+    },
+    images: {
+      domains: [
+        "localhost",
+      ],
+    },
+  };
+  
